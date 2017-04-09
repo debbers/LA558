d441cf7573b121a331d060ef180f8f4c42fa0938
debbie <deb@debbers.com>
2017-04-09 16:57:19 -0500

    quiz stuff

---
+++ quiz12/quizData.js
@@ -0,0 +1,25 @@
+var quizData = [
+    {
+        "type": "Feature",
+        "properties": {
+            "FIPS": "19118",
+            "attributes": {
+                "eSchools": 12,
+                "mSchools": 4,
+                "hSchools": 3
+            }
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "FIPS": "19119",
+            "attributes": {
+                "eSchools": 16,
+                "mSchools": 6,
+                "hSchools": 5
+                
+            }
+        }
+    }
+]
